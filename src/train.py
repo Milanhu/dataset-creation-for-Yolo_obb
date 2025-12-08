2d3fdeab707c6c1aa6407ade961af312070843d2
Milanhu <148150242+Milanhu@users.noreply.github.com>
2025-12-08 21:46:34 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,14 @@
+import torch
+from ultralytics import YOLO
 
+model = YOLO("yolo11s-obb.pt")  # will auto-download
+
+results = model.train(
+    data="data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=8,
+    device=0,                 # use GPU assuming it's available
+    project="results", 
+    name="y11s_obb_local_train_50epochs"
+)
